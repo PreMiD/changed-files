d75f05a666a90d7d9bb9468557f30ea0096d56b4
Lewis Crichton <lewistehminerz@outlook.com>
2020-05-28 15:01:38 +0100

    join by newlines not spaces

--- src/main.ts
+++ src/main.ts
@@ -76,9 +76,9 @@ async function run(): Promise<void> {
         const changedFiles = await getChangedFiles(client, pr.number, pr.changed_files)
         core.debug(`Found ${changedFiles.count} changed files for pr #${pr.number}`)
 
-        core.setOutput("files_created", changedFiles.created.join(" "))
-        core.setOutput("files_updated", changedFiles.updated.join(" "))
-        core.setOutput("files_deleted", changedFiles.deleted.join(" "))
+        core.setOutput("files_created", changedFiles.created.join("\n"))
+        core.setOutput("files_updated", changedFiles.updated.join("\n"))
+        core.setOutput("files_deleted", changedFiles.deleted.join("\n"))
     } catch (error) {
         core.setFailed(error.message)
     }
